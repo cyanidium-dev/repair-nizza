d77015c7a9de5c0a145ecd3fd73c25961e72f98c
unknown <artur01049718@gmail.com>
2025-06-05 14:30:32 +0200

    styled portfolio section for desk

--- src/components/homepage/PortfolioSection.jsx
+++ src/components/homepage/PortfolioSection.jsx
@@ -22,7 +22,6 @@ const PortfolioSection = () => {
 
     window.addEventListener("resize", handleResize);
 
-    // Убираем слушатель при размонтировании
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -33,37 +32,37 @@ const PortfolioSection = () => {
   return (
     <div
       style={{ backgroundImage: `url(${bgImage})` }}
-      className="bg-cover bg-center h-[866px] w-full mx-auto"
+      className="bg-cover bg-center h-[866px] w-full mx-auto md:h-[724px]"
     >
       <Container>
-        <div className="pt-[72px] mb-12">
-          <h2 className="font-arsenal font-normal text-4xl text-primary-white text-center mb-[19px] mx-auto uppercase">
+        <div className="pt-[72px] mb-12 md:flex md:justify-between md:mb-10 lg:items-center lg:mb-[68px]">
+          <h2 className="font-arsenal font-normal text-4xl text-primary-white text-center mb-[19px] mx-auto uppercase md:mb-0 md:text-left lg:text-5xl lg:mx-0 md:order-1">
             {t("portfolioSection.title")}
           </h2>
-          <p className="font-montserrat font-light text-sm text-primary-white text-center w-[242px] mx-auto mb-11 md:w-[300px]">
+          <p className="font-montserrat font-light text-sm text-primary-white text-center w-[242px] mx-auto mb-11 md:w-[330px] md:mb-0 md:text-right lg:text-left md:order-3 lg:mx-0 lg:text-xl">
             {t("portfolioSection.description")}
           </p>
-          <button className="w-[310px] h-[52px] rounded-[32px] bg-primary-white text-primary-black font-montserrat font-normal text-sm mx-auto leading-5 block hover:bg-primary-black hover:text-primary-white transition-all duration-300">
+          <button className="w-[310px] h-[52px] rounded-[32px] bg-primary-white text-primary-black font-montserrat font-normal text-sm mx-auto leading-5 block md:mb-0 md:order-2 lg:w-[258px] hover:bg-primary-black hover:text-primary-white transition-all duration-300">
             {t("portfolioSection.button")}
           </button>
         </div>
-        <div className="flex justify-center gap-6">
+        <div className="flex justify-center gap-6 lg:gap-5">
           <div className="mb-[26px] relative">
             <Image
               src={portfolioMob}
               alt="portfolio image"
-              className="rounded-t-[20px] md:w-[345px]"
+              className="rounded-t-[20px] md:w-[345px] lg:w-[387px] lg:h-[247px]"
             />
-            <div className="w-[310px] md:w-[345px] rounded-b-[20px] backdrop-blur-[26px] shadow-[inset_0_4px_13px_0_rgba(255,255,255,0.25)] bg-[rgba(18,18,18,0.26)] py-7 pl-6 pr-10">
-              <h3 className="font-arsenal font-normal text-base text-primary-white leading-[19px] w-[174px] uppercase mb-4">
+            <div className="w-[310px] md:w-[345px] lg:w-[387px] rounded-b-[20px] backdrop-blur-[26px] shadow-[inset_0_4px_13px_0_rgba(255,255,255,0.25)] bg-[rgba(18,18,18,0.26)] py-7 pl-6 pr-10">
+              <h3 className="font-arsenal font-normal text-base text-primary-white leading-[19px] w-[174px] uppercase mb-4 lg:text-xl lg:w-[246px]">
                 Наш последний выполненный проект
               </h3>
-              <p className="font-montserrat font-light text-xs text-primary-white">
+              <p className="font-montserrat font-light text-xs lg:text-sm text-primary-white">
                 Создаём пространство, в котором хочется жить! Мы создали
                 уникальный дизайн для дома в центре Ниццы...
               </p>
             </div>
-            <button className="absolute top-[217px] md:top-[245px] right-[24px] w-[55px] h-[55px] flex items-center justify-center bg-primary-white rounded-full hover:scale-110 transition-all duration-300">
+            <button className="absolute top-[217px] md:top-[245px] lg:top-[215px] right-[24px] w-[55px] h-[55px] flex items-center justify-center bg-primary-white rounded-full hover:scale-110 transition-all duration-300">
               <Image src={arrowDiagonal} alt="arrow button" />
             </button>
           </div>
@@ -71,23 +70,42 @@ const PortfolioSection = () => {
             <Image
               src={portfolioMob}
               alt="portfolio image"
-              className="rounded-t-[20px] md:w-[345px]"
+              className="rounded-t-[20px] md:w-[345px] lg:w-[387px] lg:h-[247px]"
             />
-            <div className="w-[310px] md:w-[345px] backdrop-blur-[26px] shadow-[inset_0_4px_13px_0_rgba(255,255,255,0.25)] bg-[rgba(18,18,18,0.26)] py-7 pl-6 pr-10">
-              <h3 className="font-arsenal font-normal text-base text-primary-white leading-[19px] w-[174px] uppercase mb-4">
+            <div className="w-[310px] md:w-[345px] lg:w-[387px] backdrop-blur-[26px] shadow-[inset_0_4px_13px_0_rgba(255,255,255,0.25)] bg-[rgba(18,18,18,0.26)] py-7 pl-6 pr-10">
+              <h3 className="font-arsenal font-normal text-base text-primary-white leading-[19px] w-[174px] uppercase mb-4 lg:text-xl lg:w-[246px]">
                 Наш последний выполненный проект
               </h3>
-              <p className="font-montserrat font-light text-xs text-primary-white">
+              <p className="font-montserrat font-light text-xs lg:text-sm text-primary-white">
                 Создаём пространство, в котором хочется жить! Мы создали
                 уникальный дизайн для дома в центре Ниццы...
               </p>
             </div>
-            <button className="absolute top-[217px] md:top-[245px] right-[24px] w-[55px] h-[55px] flex items-center justify-center bg-primary-white rounded-full hover:scale-110 transition-all duration-300">
+            <button className="absolute top-[217px] md:top-[245px] lg:top-[215px] right-[24px] w-[55px] h-[55px] flex items-center justify-center bg-primary-white rounded-full hover:scale-110 transition-all duration-300">
+              <Image src={arrowDiagonal} alt="arrow button" />
+            </button>
+          </div>
+          <div className="mb-[26px] relative hidden lg:block">
+            <Image
+              src={portfolioMob}
+              alt="portfolio image"
+              className="rounded-t-[20px] md:w-[345px] lg:w-[387px] lg:h-[247px]"
+            />
+            <div className="w-[310px] md:w-[345px] lg:w-[387px] backdrop-blur-[26px] shadow-[inset_0_4px_13px_0_rgba(255,255,255,0.25)] bg-[rgba(18,18,18,0.26)] py-7 pl-6 pr-10">
+              <h3 className="font-arsenal font-normal text-base text-primary-white leading-[19px] w-[174px] uppercase mb-4 lg:text-xl lg:w-[246px]">
+                Наш последний выполненный проект
+              </h3>
+              <p className="font-montserrat font-light text-xs lg:text-sm text-primary-white">
+                Создаём пространство, в котором хочется жить! Мы создали
+                уникальный дизайн для дома в центре Ниццы...
+              </p>
+            </div>
+            <button className="absolute top-[217px] md:top-[245px] lg:top-[215px] right-[24px] w-[55px] h-[55px] flex items-center justify-center bg-primary-white rounded-full hover:scale-110 transition-all duration-300">
               <Image src={arrowDiagonal} alt="arrow button" />
             </button>
           </div>
         </div>
-        <div className="flex justify-center gap-6 md:gap-10">
+        <div className="flex justify-center gap-6 md:gap-10 lg:hidden">
           <div className="border border-primary-white rounded-full w-[54px] h-[54px] flex items-center justify-center hover:bg-primary-white group transition-all duration-300">
             <button className="bg-transparent border border-primary-white rounded-full w-[27px] h-[27px] flex items-center justify-center group-hover:border-primary-black transition-all duration-300">
               <Image
