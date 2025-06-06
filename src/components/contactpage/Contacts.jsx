a2dff50fb3c173c4536777f73a0d1aaf8942fc0c
unknown <artur01049718@gmail.com>
2025-06-06 21:25:57 +0200

    contacts page styled for desk

--- src/components/contactpage/Contacts.jsx
+++ src/components/contactpage/Contacts.jsx
@@ -6,6 +6,7 @@ import { useTranslations } from "next-intl";
 import imgMob from "../../../public/images/image/contacts-page-mob.webp";
 import imgDesk from "../../../public/images/image/contacts-page-desk.webp";
 import imgMotifMob from "../../../public/images/image/contacts-page-motif-mob.png";
+import imgMotifDesk from "../../../public/images/image/contacts-page-motif-desk.png";
 import imgShadowMob from "../../../public/images/image/contacts-page-shadow-mob.png";
 
 const Contacts = () => {
@@ -13,16 +14,16 @@ const Contacts = () => {
 
   return (
     <Container>
-      <div className="pt-[34px] pb-[94px]">
-        <div className="mb-[110px] md:mb-[70px]">
-          <h1 className="font-arsenal font-normal text-[32px] md:text-4xl text-primary-black uppercase mb-4 md:text-center">
+      <div className="pt-[34px] lg:pt-[54px] pb-[94px] lg:pb-[150px] lg:flex">
+        <div className="mb-[110px] md:mb-[70px] lg:mr-[182px]">
+          <h1 className="font-arsenal font-normal text-[32px] md:text-4xl text-primary-black uppercase mb-4 md:text-center lg:text-left lg:text-5xl lg:mb-5">
             {t("title")}
           </h1>
-          <p className="font-montserrat font-light text-xs md:text-sm text-primary-black leading-[15px] md:text-center">
+          <p className="font-montserrat font-light text-xs md:text-sm lg:text-base text-primary-black leading-[15px] md:text-center lg:text-left">
             {t("description")}
           </p>
         </div>
-        <div className="relative">
+        <div className="relative lg:flex lg:flex-col-reverse lg:items-center">
           <Image
             src={imgMob}
             alt="interior image"
@@ -31,20 +32,25 @@ const Contacts = () => {
           <Image
             src={imgDesk}
             alt="interior image"
-            className="hidden rounded-lg mb-[100px] md:block md:mx-auto"
+            className="hidden rounded-lg mb-[100px] lg:mb-0 md:block md:mx-auto"
           />
           <Image
             src={imgMotifMob}
             alt="interior image"
-            className="absolute top-[50px] right-[50px] rotate-[10deg] md:top-[250px] md:right-[210px] -z-10"
+            className="absolute top-[50px] right-[50px] rotate-[10deg] md:top-[250px] md:right-[210px] -z-10 lg:hidden"
+          />
+          <Image
+            src={imgMotifDesk}
+            alt="interior image"
+            className="hidden absolute top-0 right-[147px] -z-10 lg:block"
           />
           <Image
             src={imgShadowMob}
             alt="interior image"
-            className="absolute top-[110px] md:top-[310px] right-[85px] md:right-[245px] -z-10"
+            className="absolute top-[110px] md:top-[310px] right-[85px] md:right-[245px] -z-10 lg:top-[-40px] lg:right-[195px] lg:rotate-[-40deg] opacity-70"
           />
-          <ul className="flex flex-col gap-8 md:flex-row md:justify-between">
-            <div className="flex flex-col gap-8">
+          <ul className="flex flex-col gap-8 md:flex-row md:justify-between lg:w-[679px] lg:mb-[90px]">
+            <div className="flex flex-col gap-8 lg:gap-[52px]">
               <li>
                 <p className="font-montserrat font-light mb-[13px] text-sm text-primary-black uppercase">
                   {t("contacts.email")}
@@ -68,7 +74,7 @@ const Contacts = () => {
                 </a>
               </li>
             </div>
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-8 lg:gap-[52px]">
               <li>
                 <p className="font-montserrat font-light mb-[13px] text-sm text-primary-black uppercase">
                   {t("contacts.telegram")}
