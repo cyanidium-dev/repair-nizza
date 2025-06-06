faed4ad42e3cabc9a39d996a7db33c1816e37a17
unknown <artur01049718@gmail.com>
2025-06-06 14:59:53 +0200

    footer creation in progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,46 +4,52 @@ import React from "react";
 import Container from "./Container";
 import Link from "next/link";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 import instagram from "../../public/images/SVG/instagram.svg";
 import telegram from "../../public/images/SVG/telegram.svg";
 import tikTok from "../../public/images/SVG/tik-tok.svg";
+import icon from "../../public/images/SVG/footer-icon.svg";
 
 const Footer = () => {
+  const t = useTranslations("footer");
+
   return (
     <footer className="bg-primary-black">
       <Container>
         <div className="pt-20 pb-[30px]">
           <div>
-            <Link href="/">Solide renovation</Link>
-            <p>Мы создаем лучшие решения в сфере ремонта</p>
+            <Link href="/" className="font-arsenal font-normal">
+              Solide renovation
+            </Link>
+            <p>{t("slogan")}</p>
           </div>
           <nav>
             <ul>
               <li>
-                <Link href="/">Главная</Link>
+                <Link href="/">{t("navigation.main")}</Link>
               </li>
               <li>
-                <Link href="/">Оставить заявку</Link>
+                <Link href="/">{t("navigation.request")}</Link>
               </li>
               <li>
-                <Link href="/">Портфолио</Link>
+                <Link href="/">{t("navigation.portfolio")}</Link>
               </li>
               <li>
-                <Link href="/">Контакты</Link>
+                <Link href="/">{t("navigation.contacts")}</Link>
               </li>
             </ul>
             <ul>
               <li>
-                <Link href="/">Юридические данные</Link>
+                <Link href="/">{t("legal.data")}</Link>
               </li>
               <li>
-                <Link href="/">Публичный договор</Link>
+                <Link href="/">{t("legal.contract")}</Link>
               </li>
               <li>
-                <Link href="/">Оферта</Link>
+                <Link href="/">{t("legal.offer")}</Link>
               </li>
               <li>
-                <Link href="/">Конфиденциальность</Link>
+                <Link href="/">{t("legal.privacy")}</Link>
               </li>
             </ul>
           </nav>
@@ -69,6 +75,13 @@ const Footer = () => {
             </li>
           </ul>
           <p>&copy; 2025 Solide renovation</p>
+          <div>
+            <p>{t("createdBy")}</p>
+            <div>
+              <p>Code-site.art</p>
+              <Image src={icon} alt="icon" />
+            </div>
+          </div>
         </div>
       </Container>
     </footer>
